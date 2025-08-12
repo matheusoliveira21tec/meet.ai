df4fda9758c4e89f7f2631ad2c31d21c14c96b0e
monegasco <mhoa.2000@gmail.com>
2025-08-12 12:28:01 -0300

    authentication socials

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,8 +1,16 @@
+import { auth } from "@/lib/auth";
 import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const Page = () => {
-  return (
-      <SignUpView/>
-  );
+const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!!session) {
+    // User is authenticated, redirect to home page
+    redirect("/");
+  }
+  return <SignUpView />;
 };
 export default Page;
